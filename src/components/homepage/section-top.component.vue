84e80249fb7c863518fde2037b5cdfef74fe98ac
Mittelblut9 <sauer.benedikt@outlook.de>
2023-10-11 19:50:42 +0200

    feat: top section

---
+++ src/components/homepage/section-top.component.vue
@@ -0,0 +1,31 @@
+<template>
+    <section class="section-top">
+        <div class="section-top--background-filter"></div>
+        <div class="section-top--text">
+            <h1><p>{{ snippets.sectionTop.title.hey }}</p>{{ snippets.sectionTop.title.intro }}</h1>
+            <h3><span class="text-highlight-yellow">{{ snippets.sectionTop.subtitle.editor }}</span> {{ snippets.sectionTop.subtitle.text }}</h3>
+        </div>
+    </section>
+</template>
+
+<script>
+export default {
+    name: 'SectionTop',
+    data() {
+        return {
+            snippets: {
+                sectionTop: {
+                    title: {
+                        hey: 'Hey!',
+                        intro: 'Ich bin Nico',
+                    },
+                    subtitle: {
+                        editor: 'Video Editor',
+                        text: 'für Creator, E-Sportler & Unternehmen'
+                    },
+                },
+            }
+        };
+    },
+};
+</script>
--- src/views/Homepage.vue
+++ src/views/Homepage.vue
@@ -1,14 +1,18 @@
 <template>
   <HeaderComponent />
+  <SectionTopComponent />
 </template>
 
 <script>
 import HeaderComponent from '@/components/homepage/header.component.vue';
+import SectionTopComponent from '@/components/homepage/section-top.component.vue';
+
 export default {
   // eslint-disable-next-line vue/multi-word-component-names
   name: 'Homepage',
   components: {
     HeaderComponent,
+    SectionTopComponent
   }
 };
 </script>
