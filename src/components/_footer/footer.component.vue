0591d350f6170181adf286e50c0c9ea05696d323
Mittelblut9 <sauer.benedikt@outlook.de>
2023-10-11 22:42:20 +0200

    feat: footer section

---
+++ src/components/_footer/footer.component.vue
@@ -0,0 +1,33 @@
+<template>
+    <footer class="section-footer">
+        <hr />
+        <p>
+            <a href="/privacy" class="link link--footer">
+                {{ snippets.footer.privacy }}
+            </a>
+            |
+            <a href="/imprint" class="link link--footer">
+                {{ snippets.footer.imprint }}
+            </a>
+            |
+            <span class="footer__copy">&copy; {{ year }} {{ snippets.footer.copy }}</span>
+        </p>
+    </footer>
+</template>
+
+<script>
+export default {
+    name: 'FooterComponent',
+    data() {
+        return {
+            year: new Date().getFullYear(),
+            snippets: {
+                footer: {
+                    privacy: 'Datenschutz',
+                    imprint: 'Impressum',
+                }
+            }
+        };
+    }
+};
+</script>
--- src/views/Homepage.vue
+++ src/views/Homepage.vue
@@ -4,6 +4,7 @@
   <ClientsComponent />
   <AboutComponent />
   <ContactComponents />
+  <FooterComponent />
 </template>
 
 <script>
@@ -12,6 +13,7 @@ import HeaderComponent from '@/components/homepage/header.component.vue';
 import SectionTopComponent from '@/components/homepage/section-top.component.vue';
 import ContactComponents from '@/components/_contact/contact.component.vue';
 import AboutComponent from '@/components/homepage/about.component.vue';
+import FooterComponent from '@/components/_footer/footer.component.vue';
 
 export default {
   // eslint-disable-next-line vue/multi-word-component-names
@@ -22,6 +24,7 @@ export default {
     ClientsComponent,
     AboutComponent,
     ContactComponents,
+    FooterComponent,
   }
 };
 </script>
