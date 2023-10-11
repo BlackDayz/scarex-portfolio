b430e46b360e9f241a7abf20e015cf0ba8e3762a
Mittelblut9 <sauer.benedikt@outlook.de>
2023-10-11 22:02:58 +0200

    feat: contact section

---
+++ src/components/_contact/contact.component.vue
@@ -0,0 +1,18 @@
+<template>
+    <section class="section-contact">
+        <h2><span class="text-highlight-yellow">Überzeugt?</span></h2>
+        <a :href="`mailto:${email}`">{{ email }}</a>
+    </section>
+</template>
+
+<script>
+export default {
+    // eslint-disable-next-line
+    name: 'ContactComponent',
+    data() {
+        return {
+            email: 'business.scarex@gmail.com'
+        };
+    },
+};
+</script>
--- src/views/Homepage.vue
+++ src/views/Homepage.vue
@@ -2,12 +2,14 @@
   <HeaderComponent />
   <SectionTopComponent />
   <ClientsComponent />
+  <ContactComponents />
 </template>
 
 <script>
 import ClientsComponent from '@/components/homepage/clients.component.vue';
 import HeaderComponent from '@/components/homepage/header.component.vue';
 import SectionTopComponent from '@/components/homepage/section-top.component.vue';
+import ContactComponents from '@/components/_contact/contact.component.vue';
 
 export default {
   // eslint-disable-next-line vue/multi-word-component-names
@@ -16,6 +18,7 @@ export default {
     HeaderComponent,
     SectionTopComponent,
     ClientsComponent,
+    ContactComponents,
   }
 };
 </script>
