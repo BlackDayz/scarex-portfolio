0bee15d1a91b0348cd1e485b65ac692b505eb8f1
Mittelblut9 <sauer.benedikt@outlook.de>
2023-10-26 10:26:56 +0200

    fix: remove public path

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,4 @@
 const { defineConfig } = require('@vue/cli-service');
 module.exports = defineConfig({
   lintOnSave: false,
-  publicPath: process.env.NODE_ENV === 'production' ? '/scarex-portfolio/' : '/',
 });
